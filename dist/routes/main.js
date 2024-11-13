b629471f442f8b6d8ffd4c32a76a4c6de753882f
Stodulski <maxisto043@gmail.com>
2024-11-13 20:52:29 -0300

    Project upload

--- dist/routes/main.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const express_1 = require("express");
-const router = (0, express_1.Router)();
-const url_controllers_1 = require("../controllers/url.controllers");
-router.get('/:shortUrl', url_controllers_1.getUrl);
-router.post("/shorturl", url_controllers_1.shortNewUrl);
-exports.default = router;
--- dist/utils/createShortName.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const createShortName = (length = 5) => {
-    const characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-    return Array.from({ length }, () => characters[Math.floor(Math.random() * characters.length)]).join("");
-};
-exports.default = createShortName;
